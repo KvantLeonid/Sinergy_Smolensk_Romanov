914c5bd67033b566beae8941ed2ba1fa06bd2743
Allaiss <razina.alisa@gmail.com>
2023-04-21 17:25:07 +0300

    second commit

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
-text_file = open("test.txt", "a")
-text_file.write(" от Леонида")
-text_file.close()
-
+from random import randint
+s =[randint(160, 196) for x in range(25)]
+print (s)
+max = max(s)
+for i in range(len(s)):
+    for g in range (0,len(s)-i-1);
+    if s[i] < s[i+1]:
+    s[i], s[i+1] = s[i+1], s[i]
+    print (s)
