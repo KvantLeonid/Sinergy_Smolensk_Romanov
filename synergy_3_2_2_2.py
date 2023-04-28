26d8b45bd8f37a622324d50bf1afcd54274371f8
KvantLeonid <kvantleorom@gmail.com>
2023-04-28 17:54:25 +0300

    file synergy_3_2_2_2

---
+++ synergy_3_2_2_2.py
@@ -0,0 +1,20 @@
+#Используем полиморфизм
+class Parrot:
+    def fly(self):
+        print("Попугай умеет летать")
+    def swim(self):
+        print("Попугай не умеет плавать")
+class Penguin:
+    def fly(self):
+        print("Пингвин не умеет летать")
+    def swim(self):
+        print("Пингвин умеет плавать")
+#общий интерфейс
+def flying_test(bird):
+    bird.fly()
+#создаем экземпляры класса
+kesha = Parrot()
+peggy = Penguin()
+#передача объектов в качестве аргумента
+flying_test(kesha)
+flying_test(peggy)
